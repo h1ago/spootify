603a86b5ac8aeac2d82213c61e7f40221d516731
Hiago Pereira <hspereira39@gmail.com>
2022-02-22 14:37:31 -0300

    alteração de rotas

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Routes from './routes'
 import Default from './layouts/Default'
 import Login from "./layouts/Login"
 
@@ -8,8 +7,8 @@ export default function App(){
     const [authenticated, setAuthenticated] = React.useState(false)
 
 
-    /* if(!authenticated)
-        return <Login/> */
+    if(!authenticated)
+        return <Login/>
 
     return <Default/>
     
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from "react-router-dom"
 import { createGlobalStyle } from 'styled-components'
 import resetCss from 'styled-reset'
 import App from './app'
@@ -13,14 +12,8 @@ const GlobalStyle = createGlobalStyle`
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    
-    <BrowserRouter>
-    
-      <App/>
-
-    </BrowserRouter>
-    
+    <GlobalStyle />    
+      <App/>    
   </React.StrictMode>,
   document.getElementById('root')
 );
